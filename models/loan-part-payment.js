b3218cca6f4b857fd2a4b6a11c4e54d02bbceee8
KaushalS1911 <kaushalsojitra19@gmail.com>
2024-10-14 16:55:52 +0530

    update interest loan payment details update part release details

--- models/loan-part-payment.js
+++ models/loan-part-payment.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const loanPartPaymentSchema = new mongoose.Schema({
     loan: {type: String, ref: "Issued Loan", required: true},
     amountPaid: Number,
+    date: Date,
     remark: String,
     paymentDetail: Object,
     deleted_at: {type: Date, default: null}
