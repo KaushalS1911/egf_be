087377bd19efab0da1fa332d6ce57254208c31b2
KaushalS1911 <kaushalsojitra19@gmail.com>
2024-12-23 17:01:02 +0530

    daily report

--- models/config.js
+++ models/config.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const configSchema = new mongoose.Schema({
     company: {type: String, ref: "Company", required: true},
     goldRate: Number,
+    headersConfig: {},
     businessType: [],
     loanTypes: [],
     permissions: {},
