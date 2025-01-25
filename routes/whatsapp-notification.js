30293557e130e54d5825f64a89f6cfca50d4643b
KaushalS1911 <kaushalsojitra19@gmail.com>
2025-01-25 18:52:47 +0530

    whatsapp api message for customer onboarding

--- routes/whatsapp-notification.js
+++ routes/whatsapp-notification.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const {sendWhatsAppNotification} = require( "../controllers/common");
+const multer = require("multer");
 
 const router = express.Router();
+const upload = multer({ storage: multer.diskStorage });
 
-router.post('/',  sendWhatsAppNotification)
+router.post('/',  upload.single("file"), sendWhatsAppNotification)
 
 module.exports = router;
