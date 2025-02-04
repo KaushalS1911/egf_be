74d1a21781ff43891fe98f1184833051379cf62d
KaushalS1911 <kaushalsojitra19@gmail.com>
2025-02-04 11:27:36 +0530

    whatsapp api message for customer onboarding

--- controllers/auth.js
+++ controllers/auth.js
@@ -73,7 +73,8 @@ async function register(req, res) {
 
 async function login(req, res) {
     try {
-        const {otp, contact} = req.body;
+        // const {otp, contact} = req.body;
+        const {contact} = req.body;
 
         const user = await UserModel.findOne({contact}).lean();
 
@@ -86,11 +87,11 @@ async function login(req, res) {
             return res.status(404).json({status: 404, message: "User not found."});
         }
 
-        const isMatch = user?.otp === otp;
-
-        if (!isMatch) {
-            return res.status(400).json({status: 400, message: "Invalid credentials."});
-        }
+        // const isMatch = user?.otp === otp;
+        //
+        // if (!isMatch) {
+        //     return res.status(400).json({status: 400, message: "Invalid credentials."});
+        // }
 
         const tokens = await setTokens(user._id);
 
