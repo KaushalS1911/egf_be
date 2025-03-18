aa9c6fe749ad6203c96986fdb75ff8036224b850
KaushalS1911 <kaushalsojitra19@gmail.com>
2025-03-18 17:37:46 +0530

    interest loan report issue fixed

--- controllers/report.js
+++ controllers/report.js
@@ -134,7 +134,7 @@ const loanSummary = async (req, res) => {
         const {companyId} = req.params;
 
         const loans = await IssuedLoanModel.find({company: companyId, deleted_at: null})
-            .populate({path: "customer", populate: "branch"}).populate("issuedBy").populate("scheme")
+            .populate({path: "customer", populate: "branch"}).populate("issuedBy").populate("scheme").populate("closedBy")
 
         const result = await Promise.all(loans.map(async (loan) => {
             loan = loan.toObject(); // Convert Mongoose document to a plain object
