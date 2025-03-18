bb56aacccd4c0f30fbea8f893601d5afd6eba0a6
KaushalS1911 <kaushalsojitra19@gmail.com>
2025-03-18 12:13:53 +0530

    interest loan report issue fixed

--- controllers/report.js
+++ controllers/report.js
@@ -251,16 +251,14 @@ const otherLoanSummary = async (req, res) => {
 
             loan.totalInterestAmt = interests.reduce((sum, entry) => sum + (entry.payAfterAdjust || 0), 0);
             // Interest & Penalty Calculation
-            const today = moment();
+            const today = moment(loan.date);
             const lastInstallmentDate = moment(loan.renewalDate);
-            const daysDiff = today.diff(lastInstallmentDate, 'days');
+            const daysDiff = lastInstallmentDate.diff(today, 'days');
 
             loan.day = daysDiff;
-
             const interestRate = loan.percentage
 
             loan.pendingInterest = ((loan.amount * (interestRate / 100)) * 12 * daysDiff) / 365;
-            ;
 
             return loan;
         }));
