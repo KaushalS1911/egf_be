3c9c35ed2f90891f7b093540324cd60e00c19c64
KaushalS1911 <kaushalsojitra19@gmail.com>
2025-04-04 11:06:54 +0530

    cash in hand and bank account report

--- routes/analytics.js
+++ routes/analytics.js
@@ -3,7 +3,7 @@ const {allTransactions, allBankTransactions} = require("../controllers/analytics
 
 const router = express.Router();
 
-router.get("/:companyId/transactions", allTransactions)
+router.get("/:companyId/cash-transactions", allTransactions)
 router.get("/:companyId/bank-transactions", allBankTransactions)
 
 module.exports = router;
