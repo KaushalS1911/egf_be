1db9e0090ec336dce74faae1f3a9b391cd6ec3f8
Dax-savani <dakshsavani15@gmail.com>
2025-05-14 14:05:15 +0530

    payment details

--- models/payment-in-out.js
+++ models/payment-in-out.js
@@ -4,12 +4,12 @@ const paymentInOutSchema = new mongoose.Schema({
     company: { type: String, ref: 'Company', required: true },
     branch: { type: String, ref: 'Branch', required: true },
     party: { type: String, ref: 'Party', required: true },
-    paymentMode: { type: String },
     receiptNo: { type: String },
     invoice: {type: String,default: ''},
     date: { type: Date },
     description: { type: String },
-    status: { type: String }
+    status: { type: String },
+    paymentDetails: Object
 }, { timestamps: true });
 
 module.exports = mongoose.model('PaymentInOut', paymentInOutSchema);
