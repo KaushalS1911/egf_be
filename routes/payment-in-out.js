26dfaed5d8354656b38e0067cf0fcb1168f02717
Dax-savani <dakshsavani15@gmail.com>
2025-05-14 11:01:36 +0530

    populate charge,payment

--- routes/payment-in-out.js
+++ routes/payment-in-out.js
@@ -7,11 +7,15 @@ const {
     updatePaymentInOut,
     deletePaymentInOut
 } = require('../controllers/payment-in-out');
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({storage: storage});
 
-router.post('/:companyId/payment', addPaymentInOut);
+
+router.post('/:companyId/payment',upload.single('invoice'), addPaymentInOut);
 router.get('/:companyId/payment', getAllPaymentInOut);
 router.get('/:companyId/payment/:paymentId', getSinglePaymentInOut);
-router.put('/:companyId/payment/:paymentId', updatePaymentInOut);
+router.put('/:companyId/payment/:paymentId',upload.single('invoice'), updatePaymentInOut);
 router.delete('/:companyId/payment/:paymentId', deletePaymentInOut);
 
 module.exports = router;
