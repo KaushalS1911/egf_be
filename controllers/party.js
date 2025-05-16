f6f7411f9d84469e52a896bc5a89a74f9f8637c3
KaushalSojitra1911 <sujalpaghadal17@gmail.com>
2025-05-16 15:17:12 +0530

    small change

--- controllers/party.js
+++ controllers/party.js
@@ -54,13 +54,6 @@ async function getAllParties(req, res) {
 async function updateParty(req, res) {
     try {
         const { partyId } = req.params;
-        const {branch} = req.query;
-
-        const party = await PartyModel.findOne({_id: partyId});
-
-        if (!party) {
-            return res.status(404).json({status: 404, message: "Party not found for the specified branch."});
-        }
 
         const updatedParty = await PartyModel.findByIdAndUpdate(partyId, req.body, { new: true });
 
