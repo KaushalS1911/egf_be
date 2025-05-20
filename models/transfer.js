796a80295d7de7498fef0d641bc85c744d27ddb9
KaushalSojitra1911 <sujalpaghadal17@gmail.com>
2025-05-20 11:53:32 +0530

    small changes

--- models/transfer.js
+++ models/transfer.js
@@ -1,14 +1,22 @@
 const mongoose = require('mongoose')
 
-const transfer = new mongoose.Schema({
-    company: {type: String, ref: "Company", required: true},
-    brand: {type: String, ref: "Brand", required: true},
-    transferType: String,
-    transferDate: {type: Date, default: Date.now()},
-    desc: String,
-    paymentDetails: Object,
-    status: String,
-    deleted_at: {type: Date, default: null},
-}, {timestamps: true})
+const paymentDetailsSchema = new mongoose.Schema({
+    from: mongoose.Schema.Types.Mixed,
+    to: mongoose.Schema.Types.Mixed,
+    amount: Number,
+    adjustmentType: {type: String},
+}, {_id: false});
 
-module.exports = mongoose.model("Transfer", transfer)
+const transferSchema = new mongoose.Schema({
+    company: {type: String, ref: "Company", required: true},
+    brand: {type: String, ref: "Brand", required: true,},
+    transferType: {type: String, required: true},
+    transferDate: {type: Date, default: Date.now},
+    desc: {type: String,},
+    paymentDetails: {type: paymentDetailsSchema, required: true,},
+    deleted_at: {type: Date, default: null},
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model("Transfer", transferSchema);
