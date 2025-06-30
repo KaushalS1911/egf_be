3166ac873174424bea28618df75ab9d2cbaaaea6
KaushalSojitra1911 <sujalpaghadal17@gmail.com>
2025-06-30 14:18:23 +0530

    changes

--- controllers/common.js
+++ controllers/common.js
@@ -133,6 +133,7 @@ async function interestReminders() {
         const loans = await IssuedLoanModel.find({
             nextInstallmentDate: {$lt: fromDate.toDate()},
             status: { $nin: ["Closed",'Issued'] },
+            company: "67eba3d435290b673d05adac",
             deleted_at: null,
         }).populate([
             { path: "customer", populate: "branch" },
